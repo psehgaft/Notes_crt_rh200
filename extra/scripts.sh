ad139b69bd6116f1b4ce01ecd4f51bcddb91c909
Heber Romero Tellez <psehgaft@gmail.com>
2023-09-28 12:53:09 -0400

    Update scripts.sh
    ######## HTTP Selinux

--- extra/scripts.sh
+++ extra/scripts.sh
@@ -9,3 +9,17 @@ hostnamectl set-hostname [hostname]
 
 dnf config-manager --add-repo [url]
 yum-config-manager --add-repo [url]
+
+######## HTTP Selinux
+
+dnf install -y httpd
+systemctl start httpd
+systemctl enable httpd
+
+chcon -t httpd_sys_content_t [dir-path]
+
+semanage fcontext -a -t httpd_sys_content_t '[dir-path](/.*)?'
+semanage port -a -t http_port_t -p tcp 82
+
+firewall-cmd --permanent --add-service={http,https}
+firewall-cmd --reload
