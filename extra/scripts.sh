c163eaa92087d27d3e54fa8e181b1b12bb73b6c8
Heber Romero Tellez <psehgaft@gmail.com>
2023-09-28 16:56:34 -0400

    Update scripts.sh
    ######## SWAP / Volumes

--- extra/scripts.sh
+++ extra/scripts.sh
@@ -58,8 +58,6 @@ systemctl enable chronyd
 
 ######## AutoFS
 
-## [server A]
-
 yum install autofs
 sudo systemctl enable --now autofs
 
@@ -94,7 +92,37 @@ mount -o remount,rw /sysroot
 chroot /sysroot
 passwd
 
-######## Root
+######## Resize Fs
+
+lvextend -L +[cuantity]M /dev/[vg]/[lv]
+xfs_growfs [mountpoint]
+
+######## SWAP / Volumes
+
+parted /dev/vdb mkpart myswap linux-swap 0MB 512MB
+udevadm settle
+mkswap /dev/vdb1
+swapon /dev/vdb1
+lsblk --fs /dev/vdb1
+# ... UUID
+vi /etc/fstab
+UUID=[UUID]  swap  swap  defaults  0 0
+systemctl daemon-reload
+swapon -a
+
+parted /dev/vdb mkpart [partitionname] xfs 512M 400GB
+udevadm settle
+vgcreate [volumegroup] /dev/vdb2
+lvcreate -n [volumename] -L 400M [volumegroup]
+mkfs -t xfs /dev/[volumename]/[volumegroup]
+vi /etc/fstab
+/dev/[volumename]/[volumegroup]  [mountpoint] xfs  defaults 1 2
+systemctl daemon-reload
+
+######## VDO
+
+
+
 
 
 
