2cce55cd173c3b023a5e8e3f6a8340b3e438e5c5
Heber Romero Tellez <psehgaft@gmail.com>
2023-09-28 13:47:05 -0400

    Update scripts.sh
    ######## Users

--- extra/scripts.sh
+++ extra/scripts.sh
@@ -10,6 +10,15 @@ hostnamectl set-hostname [hostname]
 dnf config-manager --add-repo [url]
 yum-config-manager --add-repo [url]
 
+######## Users
+
+groupadd [name-group]
+useradd [username]
+passwd [username]
+
+usermod -aG [name-group] [username]
+usermod -s /sbin/nologin [username]
+
 ######## HTTP Selinux
 
 dnf install -y httpd
@@ -23,3 +32,16 @@ semanage port -a -t http_port_t -p tcp 82
 
 firewall-cmd --permanent --add-service={http,https}
 firewall-cmd --reload
+
+######## CRON Job
+
+* * * * * "su -u user echo 'hello' >> log"
+
+######## Colab users / files
+
+mkdir /[dir-path]
+chown :[name-group] [dir-path]
+chmod ug+w [dir-path]
+chmod 2770 [dir-path]
+
+
